5b4dc9552414bd81362008fc957f55a8fc2dc3e0
msivass <mehmetsivass@gmail.com>
2023-03-06 21:17:40 -0500

    US01_LoginPage_Ilknur Login method added US03 Completed

--- src/test/java/com/BlueCRM/pages/US01_LoginPage_Ilknur.java
+++ src/test/java/com/BlueCRM/pages/US01_LoginPage_Ilknur.java
@@ -1,6 +1,7 @@
 package com.BlueCRM.pages;
 
 
+import com.BlueCRM.utilities.ConfigurationReader;
 import com.BlueCRM.utilities.Driver;
 //import com.fleet.utilities.Driver;
 import org.openqa.selenium.WebElement;
@@ -9,27 +10,31 @@ import org.openqa.selenium.support.PageFactory;
 
 public class US01_LoginPage_Ilknur {
 
-    public US01_LoginPage_Ilknur(){
+    public US01_LoginPage_Ilknur() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(name="USER_LOGIN")
+    @FindBy(name = "USER_LOGIN")
     public WebElement username;
 
 
-    @FindBy(name="USER_PASSWORD")
+    @FindBy(name = "USER_PASSWORD")
     public WebElement password;
 
     @FindBy(className = "login-btn")
     public WebElement login;
 
 
+   public void login(String userNameStr, String passwordStr) {
 
-   /* public void login(String userNameStr, String passwordStr) {
-        username.sendKeys(userNameStr);
-        password.sendKeys(passwordStr);
-        login.click();
-        // verification that we logged
-    }*/
+       Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+       username.sendKeys(userNameStr);
+       password.sendKeys(passwordStr);
+       login.click();
+
+    }
+
 
 }
+
+
